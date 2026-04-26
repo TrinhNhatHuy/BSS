7225472cf36627b777ccbec9dcb9826f7ee1603a
Trinh Nhat Huy <hwitn1610@gmail.com>
2026-04-26 20:45:19 +0700

    first project structure

---
+++ backend/backend-bss/src/main/java/com/bss/backend_bss/BackendBssApplication.java
@@ -0,0 +1,13 @@
+package com.bss.backend_bss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendBssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendBssApplication.class, args);
+	}
+
+}
---
+++ backend/backend-bss/src/test/java/com/bss/backend_bss/BackendBssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bss.backend_bss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendBssApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
